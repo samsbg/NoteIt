5e489e9ae3090da3377fae8ab3c22380b03e89ca
Samantha Bautista Gauna <sambautista@fb.com>
2021-07-14 01:01:52 -0500

    Log out functionality

--- app/src/main/java/com/codepath/noteit/activities/MainActivity.java
+++ app/src/main/java/com/codepath/noteit/activities/MainActivity.java
@@ -2,15 +2,54 @@ package com.codepath.noteit.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.codepath.noteit.R;
+import com.codepath.noteit.databinding.ActivityLoginBinding;
+import com.codepath.noteit.databinding.ActivityMainBinding;
+import com.parse.ParseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.ibUserIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, v);
+                popup.setOnMenuItemClickListener(MainActivity.this);
+                popup.inflate(R.menu.menu_user_icon);
+                popup.show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        Toast.makeText(this, "Selected Item: " +item.getTitle(), Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.iLogOut:
+                ParseUser.logOut();
+                Intent i  = new Intent(this, LoginActivity.class);
+                startActivity(i);
+                finish();
+                return true;
+            default:
+                return false;
+        }
     }
 }
