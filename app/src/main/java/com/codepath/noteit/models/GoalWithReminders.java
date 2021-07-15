409e0e5885e1a7086831955b9a02777f9c404746
Samantha Bautista Gauna <sambautista@fb.com>
2021-07-15 07:19:34 -0500

    GoalWithReminders model

---
+++ app/src/main/java/com/codepath/noteit/models/GoalWithReminders.java
@@ -0,0 +1,10 @@
+package com.codepath.noteit.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("GoalWithReminders")
+public class GoalWithReminders extends ParseObject {
+
+
+}
