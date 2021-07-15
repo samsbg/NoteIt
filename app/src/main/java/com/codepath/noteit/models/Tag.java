97c6be17b29875e30ae7cc85be36b67080cbc4bd
Samantha Bautista Gauna <sambautista@fb.com>
2021-07-15 06:47:09 -0500

    Tag model

--- app/src/main/java/com/codepath/noteit/models/Tag.java
+++ app/src/main/java/com/codepath/noteit/models/Tag.java
@@ -1,4 +1,15 @@
 package com.codepath.noteit.models;
 
-public class Tag {
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+
+@ParseClassName("Tag")
+public class Tag extends ParseObject {
+    private static final String KEY_CREATED_BY = "createdBy";
+
+    public ParseUser getCreatedBy() {
+        return getParseUser(KEY_CREATED_BY);
+    }
 }
