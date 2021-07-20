dd5fbed2ac2c5aa39d8d06ac28600263b8e47f42
Samantha Bautista Gauna <sambautista@fb.com>
2021-07-20 17:36:35 -0500

    Statistics activity update

--- app/src/main/java/com/codepath/noteit/activities/StatisticsActivity.java
+++ app/src/main/java/com/codepath/noteit/activities/StatisticsActivity.java
@@ -1,14 +1,32 @@
-package com.codepath.noteit;
+package com.codepath.noteit.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.codepath.noteit.databinding.ActivityStatisticsBinding;
+import com.codepath.noteit.models.User;
+import com.parse.ParseUser;
 
 public class StatisticsActivity extends AppCompatActivity {
+
+    ActivityStatisticsBinding binding;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_statistics);
+
+        binding = ActivityStatisticsBinding.inflate(getLayoutInflater());
+
+        View view = binding.getRoot();
+        setContentView(view);
+
+        user = (User) ParseUser.getCurrentUser();
+
+        binding.tvResult1.setText(Integer.toString(user.getCompleted()));
+        binding.tvResult2.setText(Integer.toString(user.getReviewed()));
+        binding.tvResult3.setText("Undetermined");
     }
 }
