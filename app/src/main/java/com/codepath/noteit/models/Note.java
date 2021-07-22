9f655455015265c7b5e09da49a5be30f3cbad85a
Samantha Bautista Gauna <sambautista@fb.com>
2021-07-22 18:21:14 -0500

    Update Note model

--- app/src/main/java/com/codepath/noteit/models/Note.java
+++ app/src/main/java/com/codepath/noteit/models/Note.java
@@ -5,18 +5,42 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import org.json.JSONArray;
+
 @ParseClassName("Note")
 public class Note extends ParseObject {
 
+    private static final String KEY_TITLE = "title";
     private static final String KEY_CONTENT = "content";
+    private static final String KEY_IMAGES = "images";
     private static final String KEY_CREATED_BY = "createdBy";
 
-    public ParseFile getContent() {
-        return getParseFile(KEY_CONTENT);
+    public String getTitle() {
+        return getString(KEY_TITLE);
     }
 
-    public void setContent(ParseFile content) {
+    public void setTitle(String content) {
+        put(KEY_TITLE, content);
+    }
+
+    public String getContent() {
+        return getString(KEY_CONTENT);
+    }
+
+    public void setContent(String content) {
         put(KEY_CONTENT, content);
+    }
+
+    public JSONArray getImages() {
+        return getJSONArray(KEY_IMAGES);
+    }
+
+    public void setImages(JSONArray content) {
+        put(KEY_IMAGES, content);
+    }
+
+    public void setCreatedBy(ParseUser user) {
+        put(KEY_CREATED_BY, user);
     }
 
     public ParseUser getCreatedBy() {
