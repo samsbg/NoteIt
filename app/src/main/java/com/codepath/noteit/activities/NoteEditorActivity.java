1f12e740c804cfdf683875c454b87cd78d1d27af
Samantha Bautista Gauna <sambautista@fb.com>
2021-07-22 18:20:36 -0500

    Set up the adapter in activity

--- app/src/main/java/com/codepath/noteit/activities/NoteEditorActivity.java
+++ app/src/main/java/com/codepath/noteit/activities/NoteEditorActivity.java
@@ -1,15 +1,22 @@
 package com.codepath.noteit.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.codepath.noteit.GridItemDecoration;
+import com.codepath.noteit.adapters.NoteImagesAdapter;
 import com.codepath.noteit.databinding.ActivityNoteEditorBinding;
+
+import org.json.JSONArray;
 
 public class NoteEditorActivity extends AppCompatActivity {
 
     ActivityNoteEditorBinding binding;
+    JSONArray images;
+    NoteImagesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,14 @@ public class NoteEditorActivity extends AppCompatActivity {
 
         View view = binding.getRoot();
         setContentView(view);
+
+        images = new JSONArray();
+        adapter = new NoteImagesAdapter(this, images);
+
+        GridLayoutManager gridLayout = new GridLayoutManager(this, 3);
+        binding.rvImages.setLayoutManager(gridLayout);
+        binding.rvImages.setHasFixedSize(true);
+        binding.rvImages.addItemDecoration(new GridItemDecoration());
+        binding.rvImages.setAdapter(adapter);
     }
 }
