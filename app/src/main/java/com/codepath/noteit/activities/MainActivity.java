30e3d68c8a459420c5f917be2aa608006d4df759
Samantha Bautista Gauna <sambautista@fb.com>
2021-07-29 04:07:29 -0500

    Main activity search functionality

--- app/src/main/java/com/codepath/noteit/activities/MainActivity.java
+++ app/src/main/java/com/codepath/noteit/activities/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,9 +18,11 @@ import android.widget.Toast;
 import com.codepath.noteit.R;
 import com.codepath.noteit.adapters.MainGoalAdapter;
 import com.codepath.noteit.adapters.MainNoteAdapter;
+import com.codepath.noteit.adapters.SearchAdapter;
 import com.codepath.noteit.databinding.ActivityMainBinding;
 import com.codepath.noteit.models.Goal;
 import com.codepath.noteit.models.Note;
+import com.codepath.noteit.models.Substring;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -30,8 +34,15 @@ import com.parse.FindCallback;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.Collections.reverse;
 
@@ -41,8 +52,12 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
 
     MainNoteAdapter noteAdapter;
     MainGoalAdapter goalAdapter;
+    SearchAdapter searchAdapter;
+
     List<Note> notes;
+    List<Note> notesSearch;
     List<Goal> goals;
+    Map<String, List<Note>> map;
 
     GoogleSignInClient googleClient;
 
@@ -52,6 +67,15 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        SearchAdapter.OnClickListener onClickListenerSearch = new SearchAdapter.OnClickListener() {
+            @Override
+            public void onItemClicked(int position) {
+                binding.etSearchMain.setText(notesSearch.get(position).getTitle());
+                notesSearch.clear();
+                searchAdapter.notifyDataSetChanged();
+            }
+        };
 
         View view = binding.getRoot();
         setContentView(view);
@@ -74,6 +98,12 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         //binding.rvGoals.setAdapter(goalAdapter);
         queryGoals();
 
+        map = new HashMap<>();
+        notesSearch = new ArrayList<>();
+        searchAdapter = new SearchAdapter(this, notesSearch, onClickListenerSearch);
+        binding.rvSearchMain.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvSearchMain.setAdapter(searchAdapter);
+
         binding.ibUserIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,6 +111,25 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
                 popup.setOnMenuItemClickListener(MainActivity.this);
                 popup.inflate(R.menu.menu_user_icon_main);
                 popup.show();
+            }
+        });
+
+        binding.etSearchMain.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.rvSearchMain.setVisibility(View.VISIBLE);
+                notesSearch.clear();
+                String text = s.toString().toLowerCase();
+                if(!text.equals("") && map.get(text) != null) {
+                    notesSearch.addAll(map.get(text));
+                }
+                searchAdapter.notifyDataSetChanged();
             }
         });
 
@@ -110,7 +159,6 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
 
     private void queryNotes() {
         ParseQuery<Note> query = ParseQuery.getQuery(Note.class);
-        query.include(Note.KEY_USER);
         query.whereEqualTo("createdBy", ParseUser.getCurrentUser());
         query.findInBackground(new FindCallback<Note>() {
             @Override
@@ -130,6 +178,21 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
     private void queryGoals() {
     }
 
+    private void querySubstrings() {
+        ParseQuery<Substring> query = ParseQuery.getQuery(Substring.class);
+        query.whereEqualTo("createdBy", ParseUser.getCurrentUser());
+        query.findInBackground(new FindCallback<Substring>() {
+            @Override
+            public void done(List<Substring> substringList, com.parse.ParseException e) {
+                if (e != null) {
+                    Log.e("MainActivity", "Issue with getting substring", e);
+                    return;
+                }
+                objectToMap(substringList.get(0).getMap());
+            }
+        });
+    }
+
     @Override
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
@@ -142,9 +205,11 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
             case R.id.iNote:
                 Intent j = new Intent(this, NoteEditorActivity.class);
                 startActivity(j);
+                return true;
             case R.id.iGoal:
                 Intent k = new Intent(this, GoalEditorActivity.class);
                 startActivity(k);
+                return true;
             case R.id.iConnectGoogle:
                 Intent l = googleClient.getSignInIntent();
                 startActivityForResult(l, RC_SIGN_IN);
@@ -173,6 +238,25 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         } catch (ApiException e) {
             Log.w("OAuth login", "signInResult:failed code=" + e.getStatusCode());
             Toast.makeText(getApplicationContext(), "Error while signing in, code="+ e.getStatusCode(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void objectToMap(JSONObject object) {
+        Iterator<String> keys = object.keys();
+        while(keys.hasNext()) {
+            String key = keys.next();
+            List<Note> listMap = new ArrayList<>();
+            try {
+                if (object.get(key) instanceof JSONArray) {
+                    for (int i = 0; i < ((JSONArray) object.get(key)).length(); i++) {
+                        Note noteObj = (Note) ((JSONArray) object.get(key)).get(i);
+                        listMap.add(noteObj);
+                    }
+                    map.put(key, listMap);
+                }
+            } catch (JSONException jsonException) {
+                jsonException.printStackTrace();
+            }
         }
     }
 }
